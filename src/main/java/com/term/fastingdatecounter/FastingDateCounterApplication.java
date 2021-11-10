21236542429106e1d51063cbecaa9c33063b79c4
Dahee Goo <heeouo@kookmin.ac.kr>
2021-11-11 02:05:51 +0900

    feat: initial settings - spring project configuration - with spring initializer - dependencies: Spring Web, Spring Data JPA, Spring Security, OAuth2 Client, Lombok, Mustache

---
+++ src/main/java/com/term/fastingdatecounter/FastingDateCounterApplication.java
@@ -0,0 +1,13 @@
+package com.term.fastingdatecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastingDateCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastingDateCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/term/fastingdatecounter/FastingDateCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.term.fastingdatecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastingDateCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
