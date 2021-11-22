5d48735d2ea857e793fdafb49977c7fbe2fa5d59
Dahee Goo <heeouo@kookmin.ac.kr>
2021-11-22 21:59:40 +0900

    chore: add validation annotations of request dto - FoodRequest: `@NotBlank` annotation     - start_date(column): CURRENT_TIME default -> not apply annotation related to null - ReviewRequest: `@NotBlank`, `@NotNull` annotation     - date(column): CURRENT_TIME default -> not apply annotation related to null

--- src/main/java/com/term/fastingdatecounter/api/food/controller/dto/FoodRequest.java
+++ src/main/java/com/term/fastingdatecounter/api/food/controller/dto/FoodRequest.java
@@ -5,30 +5,26 @@ import com.term.fastingdatecounter.api.food.domain.Food;
 import com.term.fastingdatecounter.api.user.domain.User;
 import lombok.Getter;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
 public class FoodRequest {
 
-    @NotNull
-    private User user;
-
-    @NotNull
-    @Size(max = 50)
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String name;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date startDate;
 
-    public FoodRequest(User user, Food food) {
-        this.user = user;
+    public FoodRequest(Food food) {
         this.name = food.getName();
         this.startDate = food.getStartDate();
     }
 
-    public Food toEntity() {
+    public Food toEntity(User user) {
         return Food.builder() // 유저 처리 필요
                 .user(user)
                 .name(name)
