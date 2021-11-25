8a51c24c6ecb8d815cbd50209a6ec71cde5a1583
Dahee Goo <heeouo@kookmin.ac.kr>
2021-11-26 06:35:53 +0900

    feat: update view templates (ing) - minor error fix - update food/review view templates: create, read, update templates - need to implement ajax

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into user (email, name, created_at, updated_at) values ('heeouo@kookmin.ac.kr', '구다희', now(), now());
+
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+insert into food (user_id, name, start_date, created_at, updated_at) values (1, '커피', '2021-11-25', now(), now());
+
+insert into review (food_id, date, title, content, fasted, created_at, updated_at) values (3, '2021-11-24', '배고프다', '커피가 있어야해', 1, now(), now());
+insert into review (food_id, date, title, content, fasted, created_at, updated_at) values (3, '2021-11-24', '배고프다', '커피가 있어야해', 1, now(), now());
+insert into review (food_id, date, title, content, fasted, created_at, updated_at) values (3, '2021-11-24', '배고프다', '커피가 있어야해', 1, now(), now());
+insert into review (food_id, date, title, content, fasted, created_at, updated_at) values (3, '2021-11-24', '배고프다', '커피가 있어야해', 1, now(), now());
+insert into review (food_id, date, title, content, fasted, created_at, updated_at) values (3, '2021-11-24', '배고프다', '커피가 있어야해', 1, now(), now());
