a414d9a52c14133a249f83d1e724ae0c3c6bd40b
Dahee Goo <heeouo@kookmin.ac.kr>
2021-12-09 04:17:53 +0900

    chore: change the language of error messages (EN -> KO) - because: it's sooooo unnatural to mix EN, KO... - unify language of alert messages - to KOREAN !!

--- src/main/java/com/term/fastingdatecounter/global/exception/ErrorCode.java
+++ src/main/java/com/term/fastingdatecounter/global/exception/ErrorCode.java
@@ -10,29 +10,29 @@ import java.util.stream.Stream;
 
 @Getter
 public enum ErrorCode {
-    METHOD_NOT_SUPPORTED("G01", "Request method is not supported"),
-    ACCESS_DENIED("G02", "No Authority : Access denied."),
-    INTERNAL_SERVER_ERROR("G03", "Internal server error occurred"),
+    METHOD_NOT_SUPPORTED("G01", "지원되지 않는 요청 메서드입니다."),
+    ACCESS_DENIED("G02", "접근 권한이 없습니다."),
+    INTERNAL_SERVER_ERROR("G03", "서버 에러로 요청을 처리하지 못했습니다."),
 
-    NOT_FOUND_USER("G04", "Unable to retrieve user information"),
-
-
-    EMPTY_FOOD_NAME("F01", "Food name is empty"),
-    TOO_LONG_FOOD_NAME("F02", "Food name is too long"),
-    NOT_FOUND_FOOD("F03", "Food does not exist"),
-    FUTURE_FOOD_START_DATE("F04", "Future date is not allowed"),
-    TOO_LATE_FOOD_START_DATE("F05", "Start date must be earlier than review date"),
+    NOT_FOUND_USER("G04", "유저 정보를 찾을 수 없습니다."),
 
 
-    FUTURE_REVIEW_DATE("R01", "Future review date is not allowed"),
-    EMPTY_REVIEW_TITLE("R02", "Review title is empty"),
-    TOO_LONG_REVIEW_TITLE("R03", "Review title is too long"),
-    EMPTY_REVIEW_CONTENT("R04", "Review content is empty"),
-    TOO_LONG_REVIEW_CONTENT("R05", "Review content is too long"),
-    EMPTY_REVIEW_FASTED("R06", "Whether to fast or not fasting is empty"),
-    NOT_FOUND_REVIEW("R07", "Review does not exist"),
-    ALREADY_WRITTEN_REVIEW_DATE("R08", "Review has been written on that date"),
-    EARLIER_REVIEW_DATE("R09", "Review date should be later than the start date of fasting")
+    EMPTY_FOOD_NAME("F01", "음식명을 작성해주세요."),
+    TOO_LONG_FOOD_NAME("F02", "음식명은 50자 이내여야 합니다."),
+    NOT_FOUND_FOOD("F03", "음식 정보를 찾을 수 없습니다."),
+    FUTURE_FOOD_START_DATE("F04", "단식 시작일은 현재까지만 선택할 수 있습니다."),
+    TOO_LATE_FOOD_START_DATE("F05", "단식 시작일은 리뷰보다 앞선 날짜만 가능합니다."),
+
+
+    FUTURE_REVIEW_DATE("R01", "미래 날짜는 리뷰를 남길 수 없습니다."),
+    EMPTY_REVIEW_TITLE("R02", "리뷰 제목을 작성해주세요."),
+    TOO_LONG_REVIEW_TITLE("R03", "리뷰 제목은 100자 이내여야 합니다."),
+    EMPTY_REVIEW_CONTENT("R04", "리뷰 내용을 작성해주세요."),
+    TOO_LONG_REVIEW_CONTENT("R05", "리뷰 내용은 500자 이내여야 합니다."),
+    EMPTY_REVIEW_FASTED("R06", "단식 여부를 선택해주세요."),
+    NOT_FOUND_REVIEW("R07", "리뷰 정보를 찾을 수 없습니다."),
+    ALREADY_WRITTEN_REVIEW_DATE("R08", "해당 날짜에 이미 작성된 리뷰가 있습니다."),
+    EARLIER_REVIEW_DATE("R09", "단식 시작일 이후의 리뷰만 남길 수 있습니다.")
     ;
 
     private final String code;
