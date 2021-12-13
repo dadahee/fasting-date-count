549a124701bda99cc1ba70d4f39b05ae76f242ce
Jason Yoo <66045861+JasonYoo1995@users.noreply.github.com>
2021-12-13 15:24:07 +0900

    Create load_test.js

---
+++ load_test.js
@@ -0,0 +1,21 @@
+const XMLHttpRequest = require("xhr2");
+
+let url = "http://218.38.160.4:9001";
+
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  var xhr = new XMLHttpRequest();
+  for (let i = 1; i <= 50000; i++) {
+    // GET test
+    xhr.open("GET", url, true); // true for asynchronous
+    xhr.send(null);
+    console.log("GET " + i);
+
+    await timeout(100);
+  }
+}
+
+main();
